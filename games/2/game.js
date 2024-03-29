4cff12867f869e4d6fb99ce84cc542e638c3a765
Mr. McKeen <pmckeen@saintbasilacademy.net>
2024-03-29 12:07:13 -0700

    E.T. updates

--- games/2/game.js
+++ games/2/game.js
@@ -12,22 +12,24 @@
 
     var playerXPosition = 400;
     var playerYPosition = 200;
+    var playerYvel = 0;
+    var flooryposition = 200;
+    var floorxposition = 400;
 
     function inputHandler(event) {
 
         if (event.code == 'KeyA') {
-            playerXPosition -= 15;
+            playerXPosition -= 50;
         }
         if (event.code == 'KeyD') {
-            playerXPosition += 15;
+            playerXPosition += 50;
         }
-        if (event.code == 'KeyW') {
-            playerYPosition -= 15;
+        if (event.code == 'Space') {
+            playerYvel = -20;
         }
         if (event.code == 'KeyS') {
             playerYPosition += 15;
         }
-
     }
     document.addEventListener("keypress", inputHandler);
 
@@ -68,7 +70,25 @@
         context.lineTo(x + 35, y + 100);
         context.stroke();
     }
+
+    function drawfloor(x, y) {
+
+        context.beginPath();
+        // Move to bottom of head
+        context.moveTo(x - 200, y + 100);
+        // Draw a line twice as tall as the head (40)
+        context.lineTo(x + 200, y + 100);
+        context.stroke();
+    }
+
     function gameLoop(timestamp) {
+        playerYPosition += playerYvel;
+        if(playerYPosition < 200){
+            playerYvel += 1;
+        }else{
+            playerYvel = 0;
+            playerYPosition = 200;
+        }
 
         let duration = timestamp - lastTimestamp;
         lastTimestamp = timestamp;
@@ -76,20 +96,23 @@
 
         context.clearRect(0, 0, canvas.width, canvas.height);
         context.font = "30px Arial";
-        context.fillText("FPS: " + Math.round(fps), 50, 50); // Show FPS on screen
-
+        context.fillText("hi I will hack you "+Math.round(fps),50,50); // Show FPS on screen
+        
         /// ---- Add More Code Here
         //context.beginPath();
 
         //context.closePath();
         //context.stroke();
-        ; drawPlayer(playerXPosition, playerYPosition)
+        drawPlayer(playerXPosition, playerYPosition);
+
+
+        drawfloor(floorxposition, flooryposition);
         /// ---- End
 
-        if(gameRunning){
+        if (gameRunning) {
             requestAnimationFrame(gameLoop); // Ask for next frame
-        }else{
-            document.removeEventListener("keypress",inputHandler);
+        } else {
+            document.removeEventListener("keypress", inputHandler);
         }
     }
 
